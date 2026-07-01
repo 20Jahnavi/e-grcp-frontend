48dfc5a078b168b09bed014e30547671a748ef23
20Jahnavi <vvce23cseaiml0134@vvce.ac.in>
2026-07-01 15:21:51 +0530

    fixed api urls

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,15 +1,32 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-
-  baseURL:
-    "https://dummyjson.com",
-
+  baseURL: "https://dummyjson.com",
+  timeout: 5000,
   headers: {
-    "Content-Type":
-      "application/json",
+    "Content-Type": "application/json",
   },
-
 });
+
+apiClient.interceptors.request.use(
+  (config) => {
+    console.log("Request Sent:", config.url);
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+apiClient.interceptors.response.use(
+  (response) => {
+    console.log("Response Received");
+    return response;
+  },
+  (error) => {
+    console.log("API Error:", error);
+    return Promise.reject(error);
+  }
+);
 
 export default apiClient;
